6cf70fd933a1090279327295e0533ec71c606284
lisboathecoder <gustavo.lisboa2009@gmail.com>
2026-03-02 08:36:42 -0300

    :tada: first commit

---
+++ exercicios.js
@@ -0,0 +1,44 @@
+// Básico
+// Maior de Idade
+let idade = 16;
+
+const resultado = idade >= 18 ? "Maior de idade" : "Menor de idade";
+
+console.log(`Você é ${resultado}`);
+
+// Número par ou ímpar
+const numero = 10;
+
+const resultadoNumero = numero % 2 === 0 ? "Par" : "Ímpar";
+
+console.log(`O número ${numero} é ${resultadoNumero}`);
+
+// Intermediário
+// Desconto
+
+const valorCompra = 120;
+
+const valorFinal = valorCompra >= 100 ? valorCompra * 0.9 : valorCompra;
+
+console.log(`O valor final da compra é R$${valorFinal}`);
+
+// Texto condicional
+const logado = false;
+
+const login = logado ? "Bem Vindo!" : "Faça Login para continuar";
+
+console.log(login);
+
+// 🔥 EXERCÍCIO BRABO — Ternário + Array + Lógica
+
+const usuarios = [
+  { nome: "Ana", ativo: true },
+  { nome: "Carlos", ativo: false },
+  { nome: "Bruna", ativo: true },
+];
+
+const formatado = usuarios.map(usuario => {
+  return usuario.ativo ? `${usuario.nome} - Ativo` : `${usuario.nome} - Inativo`;
+});B
+
+console.log(formatado);
---
+++ ot.js
@@ -0,0 +1,18 @@
+let status;
+let estoque = 0;
+
+// Usando if-else
+if(estoque > 0) {
+status = 'Disponível';
+} else {
+    status = 'Indisponível';
+}
+
+console.log(status);
+
+// Usando operador ternário
+let estoque2 = 10;
+b
+const status2 = estoque2 > 0 ? 'Disponível' : 'Indisponível';
+
+console.log(status2);
